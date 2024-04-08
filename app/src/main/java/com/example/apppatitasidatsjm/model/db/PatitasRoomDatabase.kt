7cc8a5f099be65f42e8a44cb0e0679e23ede181b
DAM1V03 <dam1v03idat@gmail.com>
2024-04-08 13:33:58 -0500

    Creación de BD room y Repository

--- app/src/main/java/com/example/apppatitasidatsjm/model/db/PatitasRoomDatabase.kt
+++ app/src/main/java/com/example/apppatitasidatsjm/model/db/PatitasRoomDatabase.kt
@@ -1,4 +1,35 @@
 package com.example.apppatitasidatsjm.model.db
 
-class PatitasRoomDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.apppatitasidatsjm.model.db.dao.PersonaDao
+import com.example.apppatitasidatsjm.model.db.entity.PersonaEntity
+
+@Database(entities = [PersonaEntity::class], version = 1)
+public abstract class  PatitasRoomDatabase: RoomDatabase() {
+    abstract fun personaDao():PersonaDao
+
+    companion object{
+        @Volatile
+        private var INSTANCIA: PatitasRoomDatabase? = null
+        fun getDatabase(context: Context): PatitasRoomDatabase{
+            val tempInstancia = INSTANCIA
+            if(tempInstancia != null){
+                return tempInstancia
+            }
+            synchronized(this){
+                val instancia = Room.databaseBuilder(
+                    context.applicationContext,
+                    PatitasRoomDatabase::class.java,
+                    "patitasdb"
+                ).build()
+                INSTANCIA = instancia
+                return instancia
+            }
+        }
+    }
+
+
 }
