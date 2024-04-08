0390b51656e3414e1811fc7af623f4c822a33dd8
DAM1V03 <dam1v03idat@gmail.com>
2024-04-08 13:06:12 -0500

    avance del registro de voluntario

--- app/src/main/java/com/example/apppatitasidatsjm/view/fragment/VoluntarioFragment.kt
+++ app/src/main/java/com/example/apppatitasidatsjm/view/fragment/VoluntarioFragment.kt
@@ -5,16 +5,37 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.get
 import com.example.apppatitasidatsjm.R
+import com.example.apppatitasidatsjm.databinding.FragmentVoluntarioBinding
+import com.example.apppatitasidatsjm.util.AppMensaje
+import com.example.apppatitasidatsjm.util.TipoMensaje
+import com.example.apppatitasidatsjm.viewmodel.VoluntarioViewModel
 
-class VoluntarioFragment : Fragment() {
-
+class VoluntarioFragment : Fragment(), View.OnClickListener {
+    var _binding: FragmentVoluntarioBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var voluntarioViewModel: VoluntarioViewModel
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_voluntario, container, false)
+        _binding = FragmentVoluntarioBinding.inflate(inflater, container, false)
+        voluntarioViewModel = ViewModelProvider(requireActivity())
+            .get(VoluntarioViewModel::class.java)
+        binding.btnregistrarvoluntario.setOnClickListener(this)
+        return binding.root
+    }
+    override fun onClick(p0: View?) {
+        if(binding.cbaceptarterminos.isChecked){
+            binding.btnregistrarvoluntario.isEnabled = false
+            //voluntarioViewModel.registrarVoluntario()
+        }else{
+            AppMensaje.enviarMensaje(binding.root,
+                "Acepte los términos y condiciones para ser voluntario",
+                TipoMensaje.ERROR)
+        }
     }
 
 
